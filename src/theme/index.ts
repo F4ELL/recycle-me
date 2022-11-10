4d01b10b4c25d3662ce099b88fa2af4f424712aa
F4ELL <ferreiraperes1234@gmail.com>
2022-11-09 21:10:04 -0300

    feat: initial configuration project

---
+++ src/theme/index.ts
@@ -0,0 +1,27 @@
+export default {
+    COLORS: {
+      WHITE: '#FFFFFF',
+  
+      GREEN_700: '#B9D40B',
+      GREEN_500: '#70AF1A',
+  
+      RED: '#CC2937',
+  
+      GRAY_700: '#121214',
+      GRAY_600: '#202024',
+      GRAY_500: '#29292E',
+      GRAY_300: '#9A9A9A',
+      GRAY_200: '#C4C4CC',
+      GRAY_100: '#E1E1E6',
+    },
+    FONT_FAMILY: {
+      REGULAR: 'Roboto_400Regular',
+      BOLD: 'Roboto_700Bold'
+    },
+    FONT_SIZE: {
+      SM: 14,
+      MD: 16,
+      LG: 18,
+      XL: 24
+    }
+  };
