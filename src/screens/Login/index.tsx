5bc1f90d9ea8d55df69c4639dbef1d1fc7626f47
F4ELL <ferreiraperes1234@gmail.com>
2022-11-14 17:06:28 -0300

    feat: create highlight component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '../../components/Header';
+import { Highlight } from '../../components/Highlight';
 import { Container } from './styles';
 
 export function Login() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title='Recycle.me'
+        subtitle='mais que uma obrigação, a reciclagem é uma necessidade'
+      />
     </Container>
   );
 }
