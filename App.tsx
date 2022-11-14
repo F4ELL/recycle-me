fd359e7ef06dff6d3181791d65fb854bb87584a2
F4ELL <ferreiraperes1234@gmail.com>
2022-11-14 16:15:19 -0300

    feat: setting fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import { Login } from './src/screens/Login';
 
 import theme from './src/theme'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold }) 
+
   return (
     <ThemeProvider theme={theme}>
-      <Login />
+      { fontsLoaded ? <Login /> : <Loading /> }
     </ThemeProvider>    
   );
 }
