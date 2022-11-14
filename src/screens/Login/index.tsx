7b7b9730c581c1a24086a6eecbc7ed07bfc4cbf8
F4ELL <ferreiraperes1234@gmail.com>
2022-11-14 17:45:00 -0300

    feat: create input component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
+import { Input } from '../../components/Input';
 import { Container } from './styles';
 
 export function Login() {
@@ -9,6 +10,13 @@ export function Login() {
       <Highlight 
         title='Recycle.me'
         subtitle='mais que uma obrigação, a reciclagem é uma necessidade'
+      />
+      <Input 
+        placeholder='Username'
+        style={{marginBottom: 12}}
+      />
+      <Input 
+        placeholder='Password'
       />
     </Container>
   );
