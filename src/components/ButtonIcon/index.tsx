8676ebcfc3f1379cc17e0d73dec3eb89d006534d
F4ELL <ferreiraperes1234@gmail.com>
2022-11-21 17:43:43 -0300

    feat: create buttonicon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Icon } from "./styles";
+
+export function ButtonIcon() {
+    return (
+        <Container>
+            <Icon 
+                name='close'
+            />
+        </Container>
+    )
+}
---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,18 @@
+import { TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import styled from 'styled-components/native'
+
+export const Container = styled(TouchableOpacity)`
+    width: 56px;
+    height: 56px;
+
+    justify-content: center;
+    align-items: center;
+
+    margin-left: 12px;
+`
+
+export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
+    size: 24,
+    color: theme.COLORS.RED
+}))``
